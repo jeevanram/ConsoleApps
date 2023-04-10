6f2b1df91d7dffa271c673694646751c0523abde
jeevanram <mailjeevanram@gmail.com>
2023-04-09 22:05:20 -0500

    EF Core : DB Entity added

---
+++ DataAccessEntityAndSP_Performance/DataAccessEntityAndSP_Performance/Model/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessEntityAndSP_Performance.Model;
+
+public partial class UserInfo
+{
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? City { get; set; }
+
+    public string? County { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Postalcode { get; set; }
+}
